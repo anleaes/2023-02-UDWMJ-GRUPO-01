9bd9073dacbf05388c7780f1e3a82e4fc69cfad5
Gabriel Rebes~ <you@example.com>
2023-12-07 10:04:47 -0300

    profissional update

--- teleservicosapp/apps/profissional/models.py
+++ teleservicosapp/apps/profissional/models.py
@@ -6,18 +6,24 @@ from tipodeprofissional.models import Tipodeprofissional
 class Profissional(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
-    date_fabrication = models.DateField('Data Fabricacao', auto_now=False, auto_now_add=False) 
-    is_active = models.BooleanField('Ativo', default=False)
-    photo = models.ImageField('Foto', upload_to='photos')
-    doc = models.FileField('Documentos', upload_to='docs')
-    category = models.ForeignKey(Tipodeprofissional, on_delete=models.CASCADE)
+    first_name = models.CharField('Nome', max_length=50)
+    last_name = models.CharField('Sobrenome', max_length=100) 
+    address = models.CharField('Endereco', max_length=200)   
+    cell_phone = models.CharField('Telefone celular', max_length=20)
+    email = models.EmailField('E-mail',null=False, blank=False)
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    )
+    gender = models.CharField('Genero', max_length=1, choices=GENDER_CHOICES)
+    tipo_de_profissional = models.ForeignKey(Tipodeprofissional, on_delete=models.CASCADE)
+   
     
     class Meta:
-        verbose_name = 'Profissional'
-        verbose_name_plural = 'Profissional'
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
         ordering =['id']
 
     def __str__(self):
-        return self.name
+        return self.first_name
