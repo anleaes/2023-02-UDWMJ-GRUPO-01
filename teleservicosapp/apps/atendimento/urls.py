c5add67c4f6d9368f6de88fa37a927cd6cfc8b85
GabrielRebes <glrebes@gmail.com>
2023-12-06 21:20:49 -0300

    atendimento2

--- teleservicosapp/apps/atendimento/urls.py
+++ teleservicosapp/apps/atendimento/urls.py
@@ -5,8 +5,8 @@ app_name = 'atendimento'
 
 urlpatterns = [
     path('', views.list_atendimento, name='list_atendimento'),
-    path('adicionar/<int:id_client>/', views.add_order, name='add_atendimento'),
-    path('excluir/<int:id_atendimento/', views.delete_order, name='delete_atendimento'),
+    path('adicionar/<int:id_client>/', views.add_atendimento, name='add_atendimento'),
+    path('excluir/<int:id_atendimento/', views.delete_atendimento, name='delete_atendimento'),
     path('excluir-material/<int:id_materiais_atendimento>/', views.delete_materiais_atendimento, name='delete_materiais_atendimento'),
     path('adicionar-material/<int:id_atendimento>/', views.add_materiais_atendimento, name='add_materiais_atendimento'),
     #adcionar/profissional
